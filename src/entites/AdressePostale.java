6a680a60d24399b6eedcc81bb16c4d458c02056d
Maxence LOGODIN <maxence.logodin@gmail.com>
2022-10-10 14:43:48 +0200

    modif adresse

--- src/entites/AdressePostale.java
+++ src/entites/AdressePostale.java
@@ -7,48 +7,5 @@ public class AdressePostale {
 	public int codePostale;
 	public String villeString;
 	
-	
-	public AdressePostale() {
-		super();
-	}
-	
-	public AdressePostale(int numeroDeRue, String libelleDeRueString, int codePostale, String villeString) {
-		super();
-		this.numeroDeRue = numeroDeRue;
-		this.libelleDeRueString = libelleDeRueString;
-		this.codePostale = codePostale;
-		this.villeString = villeString;
-	}
-	
-	
-	
-	public int getNumeroDeRue() {
-		return numeroDeRue;
-	}
-	public void setNumeroDeRue(int numeroDeRue) {
-		this.numeroDeRue = numeroDeRue;
-	}
-	
-	public String getLibelleDeRueString() {
-		return libelleDeRueString;
-	}
-	public void setLibelleDeRueString(String libelleDeRueString) {
-		this.libelleDeRueString = libelleDeRueString;
-	}
-	
-	public int getCodePostale() {
-		return codePostale;
-	}
-	public void setCodePostale(int codePostale) {
-		this.codePostale = codePostale;
-	}
-	
-	public String getVilleString() {
-		return villeString;
-	}
-	public void setVilleString(String villeString) {
-		this.villeString = villeString;
-	}
-	
 
 }
