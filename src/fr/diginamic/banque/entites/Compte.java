a575e090dc62a565f874990d521aadee49f9340a
Maxence LOGODIN <maxence.logodin@gmail.com>
2022-10-10 15:57:48 +0200

    Tp 5: Le sysout nous retourne le pointeur de l'instance créé

---
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,17 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+	
+	int numeroDeCompte;
+	double soldeDeCompte;
+	
+	
+	public Compte(int numeroDeCompte, double soldeDeCompte) {
+		super();
+		this.numeroDeCompte = numeroDeCompte;
+		this.soldeDeCompte = soldeDeCompte;
+	}
+	
+	
+
+}
---
+++ src/fr/diginamic/banque/entites/TestCompte.java
@@ -0,0 +1,14 @@
+package fr.diginamic.banque.entites;
+
+public class TestCompte {
+
+	public static void main(String[] args) {
+		
+		
+		Compte compte = new Compte(1654641, 10050.62);
+		
+		System.out.println(compte);
+		
+	}
+
+}
