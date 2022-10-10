c93487c36c1b3d0c31f9732ada8f739f1961f131
Maxence LOGODIN <maxence.logodin@gmail.com>
2022-10-10 15:45:55 +0200

    Tp 4

---
+++ src/utils/TestMethodeStatic.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class TestMethodeStatic {
+
+	public static void main(String[] args) {
+		
+		String chaine = "12";
+		
+		int resultat = Integer.valueOf(chaine);
+
+		int a = 2;
+		int b = 561684;
+		
+		System.out.println(Integer.max(a, b));
+	}
+
+}
